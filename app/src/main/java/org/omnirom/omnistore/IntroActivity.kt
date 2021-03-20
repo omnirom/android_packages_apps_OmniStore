d4e8f959592f69d6c288acf71fb414f01d04ae6a
maxwen <max.weninger@gmail.com>
2021-03-20 14:41:49 +0100

    OmniStore: request install permission already in intro activity
    system will always ask later again if needed
    
    Change-Id: Id54b402456d0d13ff1f7c0ed031b194ae8fdf86a

--- app/src/main/java/org/omnirom/omnistore/IntroActivity.kt
+++ app/src/main/java/org/omnirom/omnistore/IntroActivity.kt
@@ -1,13 +1,48 @@
 package org.omnirom.omnistore;
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class IntroActivity : AppCompatActivity() {
+    private val REQUEST_INSTALL_PERMS = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
-        findViewById<Button>(R.id.ok).setOnClickListener { finish() }
+        findViewById<Button>(R.id.ok).setOnClickListener {
+            if (!hasInstallPermissions()) {
+                checkUnknownResourceInstallation()
+            } else {
+                finish()
+            }
+        }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            REQUEST_INSTALL_PERMS -> {
+                finish()
+            }
+        }
+    }
+
+    private fun hasInstallPermissions(): Boolean {
+        return packageManager.canRequestPackageInstalls()
+    }
+
+    private fun checkUnknownResourceInstallation() {
+        startActivityForResult(
+            Intent(
+                Settings.ACTION_MANAGE_UNKNOWN_APP_SOURCES,
+                Uri.parse("package:${BuildConfig.APPLICATION_ID}")
+            ),
+            REQUEST_INSTALL_PERMS
+        )
+    }
+
 }
